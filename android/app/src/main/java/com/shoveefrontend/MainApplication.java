0ae47bab1d192a54377141cead97dbe60a1ad6b7
Rishabh Rawat <ngocthang1493@gmail.com>
2020-06-27 17:48:39 +0700

    buid ios

--- android/app/src/main/java/com/shoveefrontend/MainApplication.java
+++ android/app/src/main/java/com/shoveefrontend/MainApplication.java
@@ -3,6 +3,7 @@ package com.shoveefrontend;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.BV.LinearGradient.LinearGradientPackage;
 import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new LinearGradientPackage(),
             new ReactNativeOneSignalPackage(),
             new ImagePickerPackage(),
             new VectorIconsPackage(),
